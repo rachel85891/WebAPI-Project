623eae874efa2f188245285296455a1b34acafd8
seeandbesharp-code-review <38328279757@mby.co.il>
2025-12-09 18:33:21 +0200

    Code Review

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -11,48 +11,28 @@ namespace Services
         private readonly IUserRepository _repository;
 
         public UserServices(IPasswordService service, IUserRepository repository)
-<<<<<<< HEAD
-        {
-            _service = service;
-            _repository = repository;
-=======
         {
             _service = service;
             _repository = repository;
         }
 
-        public User getUserByID(int id)
+        public async Task<User> GetUserByID(int id)
         {
-            return _repository.getUserByID(id);
->>>>>>> a2a7b1af37d7aed8d93ab01dfe8282c82897ce94
+            return await _repository.GetUserByID(id);
         }
-
-        public async Task<User> getUserByID(int id)
+        public async Task<User> AddUser(User user)
         {
-<<<<<<< HEAD
-            return await _repository.getUserByID(id);
-        }
-        public async Task<User> addUser(User user)
-        {
-=======
->>>>>>> a2a7b1af37d7aed8d93ab01dfe8282c82897ce94
-            if (_service.getStrengthByPass(user.Password).strength < 2)
+            if (_service.GetStrengthByPass(user.Password).strength < 2)
             {
                 return null;
             }
-<<<<<<< HEAD
-            return await _repository.addUser(user);
+            return await _repository.AddUser(user);
         }
         public async Task<User> UpdateUser(User userToUpdate)
-=======
-            return _repository.addUser(user);
-        }
-        public User UpdateUser(User userToUpdate)
->>>>>>> a2a7b1af37d7aed8d93ab01dfe8282c82897ce94
         {
-            if (_service.getStrengthByPass(userToUpdate.Password).strength < 2)
+            if (_service.GetStrengthByPass(userToUpdate.Password).strength < 2)
                 return null;
-            _repository.UpdateUser(userToUpdate);
+            await _repository.UpdateUser(userToUpdate);
             return userToUpdate;
         }
     }
--- WebApiShop/Controllers/UsersController.cs
+++ WebApiShop/Controllers/UsersController.cs
@@ -3,61 +3,33 @@ using System.Text.Json;
 using Entities;
 using Services;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace WebApiShop.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
-        IUserServices _service;
+        private readonly IUserServices _service;
         public UsersController(IUserServices service)
-<<<<<<< HEAD
-=======
         {
             _service = service;
         }
-
-        // GET: api/<UsersController>
-        [HttpGet]
-        public string Get()
->>>>>>> a2a7b1af37d7aed8d93ab01dfe8282c82897ce94
-        {
-            _service = service;
-        }
-
-        //// GET: api/<UsersController>
-        //[HttpGet]
-        //public string Get()
-        //{
-        //    return "value";
-        //}
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>>Get(int id)
+        public async Task<ActionResult<User>> Get(int id)
         {
-<<<<<<< HEAD
-            User user= await _service.getUserByID(id);
-=======
-            User user= _service.getUserByID(id);
->>>>>>> a2a7b1af37d7aed8d93ab01dfe8282c82897ce94
+            User user= await _service.GetUserByID(id);
             if(user == null) 
-                return NoContent();
+                return NotFound();
             return Ok(user);
-
         }
         
         // POST api/<UsersController>
         [HttpPost]
-        public async Task<ActionResult<User>> POST([FromBody] User user)
+        public async Task<ActionResult<User>> Post([FromBody] User user)
         {
-<<<<<<< HEAD
-            user = await _service.addUser(user);
-=======
-            user = _service.addUser(user);
->>>>>>> a2a7b1af37d7aed8d93ab01dfe8282c82897ce94
+            user = await _service.AddUser(user);
             if (user == null)
             {
                 return BadRequest("Password is too weak!");
@@ -67,27 +39,20 @@ namespace WebApiShop.Controllers
 
         // PUT api/<UsersController>/5
         [HttpPut("{id}")]
-<<<<<<< HEAD
-        public async Task<ActionResult<User>> Put(int id,[FromBody] User userToUpdate)
+        public async Task<IActionResult> Put(int id,[FromBody] User userToUpdate)
         {
             userToUpdate.Id = id;
-            userToUpdate =await _service.UpdateUser(userToUpdate);
-=======
-        public ActionResult<User> Put(int id,[FromBody] User userToUpdate)
-        {
-            userToUpdate.Id = id;
-            userToUpdate =_service.UpdateUser(userToUpdate);
->>>>>>> a2a7b1af37d7aed8d93ab01dfe8282c82897ce94
+            userToUpdate = await _service.UpdateUser(userToUpdate);
             if (userToUpdate == null)
                 return BadRequest("Password is too weak!");
             else
-                return Ok(userToUpdate);
+                return NoContent();
         }
 
-        //// DELETE api/<UsersController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
